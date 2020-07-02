1c124e8c2dd1d08c3e2e32401c5e7927ecc30aa9
Ale <victoryforphil@gmail.com>
2020-07-02 11:21:42 -0700

    Base Project

---
+++ exporter/SynthImporterLib/SynthImporterLib/SynthImporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+using SharpGLTF.IO;
+using SharpGLTF.Schema2;
+namespace SynthImporterLib
+{
+    public class SynthImporter
+    {
+
+         void Load(string path){
+
+        public ReadContext _readContext = ReadContext.CreateFromFile(path);
+        
+        }
+    }
+}
