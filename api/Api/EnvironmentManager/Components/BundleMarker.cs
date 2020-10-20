8f30d70bf48f379648649864dd6240aed4cc58cd
Kira Corbett <kiracorbett00@gmail.com>
2020-10-20 09:56:34 -0700

    Init unity project

--- api/Api/EnvironmentManager/Components/BundleMarker.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using SynthesisAPI.Modules.Attributes;
-
-namespace SynthesisAPI.EnvironmentManager.Components {
-    public class BundleMarker : Component {
-        public string Name { get; set; }
-    }
-}
---
+++ core/Assets/Scripts/Cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Rigidbody cubeRigidBody;
+    private Vector3 inputVector;
+
+    private float moveX;
+    private float moveY;
+
+    void Start()
+    {
+        cubeRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// GetAxis zero to one progressively
+    	// GetAxisRaw zero to one snap directly
+    	print(Input.GetAxis("Horizontal"));
+
+    	// multiplier factor is __ x faster than gravity; y is gravity
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * 5, cubeRigidBody.velocity.y, Input.GetAxis("Vertical") * 5);
+        
+        // transform.position is the start + 
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+        cubeRigidBody.velocity = inputVector;
+    }
+}
