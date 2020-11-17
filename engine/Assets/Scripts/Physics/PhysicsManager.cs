202cc5470f907e2b1810a868526b20ac8c20a060
ajaypai <ajaypai@cs.washington.edu>
2020-11-16 16:33:24 -0800

    rough physics manager

---
+++ engine/Assets/Scripts/Physics/PhysicsManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Synthesis.Physics
+{
+    /// <summary>
+    /// Controls physics within unity
+    /// </summary>
+    public class PhysicsManager : MonoBehaviour
+    {
+        public static bool isPaused { get; set; } = false;
+
+        // Use this for initialization
+        void Awake()
+        {
+            UnityEngine.Physics.autoSimulation = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!isPaused) StepForward();
+        }
+
+        public static void Pause()
+        {
+            isPaused = true;
+        }
+
+        public static void Play()
+        {
+            isPaused = false;
+        }
+
+        public static void TogglePlay()
+        {
+            isPaused = !isPaused;
+        }
+        public static void StepForward()
+        {
+            UnityEngine.Physics.Simulate(Time.fixedDeltaTime);
+        }
+        public static void StepBackward()
+        {
+            //TODO: save position and rotation of objects each frame?
+        }
+    }
+}
