629c8cce4e6cd8fc58d5e835421f7ddad20831c8
KyroVibe <hunter.barclay18@gmail.com>
2021-03-14 19:19:46 -0700

    Created Hierarchy

--- engine/Assets/Scripts/UI/Hierarchy/Hierarchy.cs
+++ engine/Assets/Scripts/UI/Hierarchy/Hierarchy.cs
@@ -1,10 +1,85 @@
 ﻿using Synthesis.UI.Hierarchy.HierarchyItems;
+using UnityEngine;
 
 namespace Synthesis.UI.Hierarchy
 {
-    public static class Hierarchy
+    public class Hierarchy : MonoBehaviour
     {
-        public static HierarchyItem FloatingHierarchy;
-        public static HierarchyItem PanelHierarchy;
+        public static Hierarchy HierarchyInstance { get; private set; }
+
+        public HierarchyFolderItem rootFolder;
+        public static HierarchyFolderItem RootFolder => HierarchyInstance.rootFolder;
+        public GameObject folderPrefab;
+        public static GameObject FolderPrefab => HierarchyInstance.folderPrefab;
+        public GameObject itemPrefab;
+        public static GameObject ItemPrefab => HierarchyInstance.itemPrefab;
+        public GameObject contentContainer;
+        public static GameObject ContentContainer => HierarchyInstance.contentContainer;
+        public static bool Changes = true;
+
+        public float TabSize = 20f;
+
+        private void Awake() {
+            HierarchyInstance = this;
+        }
+
+        public void Start() {
+            rootFolder.Init("Scene", null);
+
+            var robots = rootFolder.CreateFolder("Robots");
+            var fields = rootFolder.CreateFolder("Fields");
+
+            robots.CreateItem("997 Spartan Robotics");
+            robots.CreateItem("1425 Error Code");
+
+            fields.CreateItem("2020 Infinite Recharge");
+
+            /*
+            RootFolder
+                Folder A
+                    Item AA
+                    Folder AA
+                        Item AAA
+                    Item AB
+                Item A
+                Folder B
+                    Item BA
+            */
+
+            // Big Test
+            /*var folderA = RootFolder.CreateFolder("Folder A");
+            var itemA = RootFolder.CreateItem("Item A");
+            var folderB = RootFolder.CreateFolder("Folder B");
+
+            var itemAA = folderA.CreateItem("Item AA");
+            var folderAA = folderA.CreateFolder("Folder AA");
+            var itemAB = folderA.CreateFolder("Item AB");
+
+            var itemAAA = folderAA.CreateItem("Item AAA");
+
+            var itemBA = folderB.CreateItem("Item BA");*/
+
+            // folderB.Remove();
+            // folderAA.Add(folderB);
+
+            // RootFolder.DebugPrint();
+        }
+
+        public void Update() {
+            if (Changes) {
+                Changes = false;
+                float heightAccum = RootFolder.GetComponent<RectTransform>().rect.height;
+                RectTransform t;
+                for (int i = 0; i < RootFolder.Items.Count; i++) {
+                    if (RootFolder.Items[i].item.Visible) {
+                        float tabSize = RootFolder.Items[i].item.Depth * TabSize;
+                        t = RootFolder.Items[i].item.GetComponent<RectTransform>();
+                        t.offsetMin = new Vector2(tabSize, t.offsetMin.y);
+                        t.localPosition = new Vector3(t.localPosition.x, -heightAccum, t.localPosition.z);
+                        heightAccum += t.rect.height;
+                    }
+                }
+            }
+        }
     }
 }
--- engine/Assets/Scripts/UI/Hierarchy/HierarchyItems/HierarchyItem.cs
+++ engine/Assets/Scripts/UI/Hierarchy/HierarchyItems/HierarchyItem.cs
@@ -1,20 +1,115 @@
-﻿using UnityEngine;
+﻿using System.Net.NetworkInformation;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using TMPro;
+
+#nullable enable
 
 namespace Synthesis.UI.Hierarchy.HierarchyItems
 {
     public class HierarchyItem : MonoBehaviour
     {
+        public delegate void OnClickEvent();
+        public event OnClickEvent OnItemClicked;
+
+        public bool IsInherited {
+            get => this.GetType() != typeof(HierarchyItem);
+        }
+
+        public int Index {
+            get {
+                if (Parent == null)
+                    return 0;
+                return Parent.GetIndex(this);
+            }
+        }
+        public int LocalIndex {
+            get {
+                if (Parent == null)
+                    return 0;
+                return Parent.GetLocalIndex(this);
+            }
+        }
+        public int GlobalIndex {
+            get {
+                if (Parent == null)
+                    return 0;
+                return Parent.GetGlobalIndex(this);
+            }
+        }
+        public int Depth {
+            get {
+                if (Parent == null)
+                    return 0;
+                return Parent.Depth + 1;
+            }
+        }
+        public TMP_Text TitleText;
+        // public Transform Root;
+        public HierarchyFolderItem? Parent;
+        private string title = String.Empty;
+        public string Title {
+            get => title;
+            set {
+                title = value;
+                TitleText.text = title;
+                gameObject.name = title;
+            }
+        }
+        private bool visible = true;
+        public bool Visible {
+            get => visible;
+            set {
+                visible = value;
+                SetVisible(visible);
+            }
+        }
+
         // Use this for initialization
-        void Start()
+        void Awake() {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(() => {
+                OnItemClicked?.Invoke();
+            });
+        }
+
+        public virtual void Init(string title, HierarchyFolderItem? parent) {
+            Title = title;
+            if (parent != null) {
+                // Parent = parent;
+                parent.Add(this);
+            }
+            Visible = true;
+            // if (Parent != null)
+            //     Root = Parent.Root;
+        }
+
+        protected virtual void SetVisible(bool visible) {
+            gameObject.SetActive(visible);
+        }
+
+        public virtual void Remove() {
+            HierarchyFolderItem parent = Parent!;
+            while (parent != null) {
+                int num = parent.Items.RemoveAll(x => x.item.gameObject == this.gameObject);
+                // Debug.Log($"{Title} removed {num} times");
+                parent = parent.Parent!;
+            }
+            Parent = null;
+            Visible = false;
+            Hierarchy.Changes = true;
+        }
+
+        /*protected virtual void InitProtected()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        public virtual int GetSize() => 1;
 
-        }
+        public void Show() => Show(!gameObject.activeSelf);
+        public virtual void Show(bool show) => gameObject.SetActive(show);*/
     }
 }
